1548f6e7fc0ea1ea944a53aa2f55c19ff6463b78
NevarDima <38244830+NevarDima@users.noreply.github.com>
2021-05-31 22:05:11 +0300

    STEP_10_JRTB-10: extended bot statistics for admins. (#26)
    * STEP_10_JRTB-10: added AdminCommand annotation and AdminHelpCommand part1
    
    * STEP_10_JRTB-10: added AdminCommand annotation and AdminHelpCommand part2
    
    * STEP_10_JRTB-10: added AdminCommand annotation and AdminHelpCommand part2
    
    * STEP_10_JRTB-10: added AdminCommand annotation and AdminHelpCommand -- fix bugs

--- src/main/java/com/github/javarushcommunity/jrtb/bot/JavaRushTelegramBot.java
+++ src/main/java/com/github/javarushcommunity/jrtb/bot/JavaRushTelegramBot.java
@@ -3,6 +3,7 @@ package com.github.javarushcommunity.jrtb.bot;
 import com.github.javarushcommunity.jrtb.command.CommandContainer;
 import com.github.javarushcommunity.jrtb.javarushclient.JavaRushGroupClient;
 import com.github.javarushcommunity.jrtb.service.GroupSubService;
+import com.github.javarushcommunity.jrtb.service.StatisticsService;
 import com.github.javarushcommunity.jrtb.service.impl.SendBotMessageServiceImpl;
 import com.github.javarushcommunity.jrtb.service.TelegramUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
 
 import static com.github.javarushcommunity.jrtb.command.CommandName.NO;
 
@@ -30,20 +33,32 @@ public class JavaRushTelegramBot extends TelegramLongPollingBot {
     private final CommandContainer commandContainer;
 
     @Autowired
-    public JavaRushTelegramBot(TelegramUserService telegramUserService, JavaRushGroupClient groupClient, GroupSubService groupSubService) {
-        this.commandContainer = new CommandContainer(new SendBotMessageServiceImpl(this),telegramUserService,groupClient,groupSubService);
+    public JavaRushTelegramBot(TelegramUserService telegramUserService,
+                               JavaRushGroupClient groupClient,
+                               GroupSubService groupSubService,
+                               @Value("#{'${bot.admins}'.split(',')}") List<String> admins,
+                               StatisticsService statisticsService) {
+
+        this.commandContainer = new CommandContainer(
+                new SendBotMessageServiceImpl(this),
+                telegramUserService,
+                groupClient,
+                groupSubService,
+                admins,
+                statisticsService);
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String message = update.getMessage().getText().trim();
+            String userName = update.getMessage().getFrom().getUserName();
             if (message.startsWith(COMMAND_PREFIX)) {
                 String commandIdentifier = message.split(" ")[0].toLowerCase();
 
-                commandContainer.retrieveCommand(commandIdentifier).execute(update);
+                commandContainer.retrieveCommand(commandIdentifier, userName).execute(update);
             } else {
-                commandContainer.retrieveCommand(NO.getCommandName()).execute(update);
+                commandContainer.retrieveCommand(NO.getCommandName(), userName).execute(update);
             }
         }
     }
